ce478ea5bbcf5802d7d32f9d1bfc345c9c146cd1
Deepika-J <step.strong.79@gmail.com>
2024-01-06 14:35:23 +0530

    logger middleware added

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import weatherSlice from './weather';
+import logger from "redux-logger";
 
 const store = configureStore({
     reducer:{
         weather:weatherSlice
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 }
 )
 
